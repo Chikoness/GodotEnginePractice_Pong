fbef362c12635f2f529a60e565a0ab18fe5081aa
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2018-05-05 21:32:44 +0800

    Implemented ball bouncing on the paddle and walls.

--- source/paddle/paddle.gd
+++ source/paddle/paddle.gd
@@ -8,7 +8,3 @@ func _process(delta):
 	#mouse tracking
 	var motion = (get_global_mouse_position().x - get_position().x) * 0.2
 	translate(Vector2(motion, 0))
-	
-func _on_area_entered(area):
-	if area.get_name() == "Ball":
-		area.direction = Vector2(randf() * 2 - 1, ball_dir).normalized()
